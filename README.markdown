53b903511222511362257d5982271c69a6d3d3d1
Sushil <ksushil7@gmail.com>
2012-01-26 03:15:20 +0530

    All Commit

---
+++ README.markdown
@@ -0,0 +1,7 @@
+    ...!!   Welcome To My First Application in Ruby on Rails  .!!
+I am just learnig and implementing my ROR learning there and there.
+
+      Regards,
+     Sushil krishna
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'aa794de818078e51d0f68c8409d4d942809d7c286d29c1214808920558508110960670c72b33711408bf31afbace961c84a9318d9658003d51bb0a41960aebff'
